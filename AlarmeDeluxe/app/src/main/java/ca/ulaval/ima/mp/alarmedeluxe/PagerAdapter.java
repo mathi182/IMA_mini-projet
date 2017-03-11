30232e32295cb567b3b9970ca49b64b3630ccb64
Mathieu Boily <mathiboily@hotmail.com>
2017-03-11 11:10:43 -0500

    Added icons for tabs and tried some color match (don't kill me plz).

--- AlarmeDeluxe/app/src/main/java/ca/ulaval/ima/mp/alarmedeluxe/PagerAdapter.java
+++ AlarmeDeluxe/app/src/main/java/ca/ulaval/ima/mp/alarmedeluxe/PagerAdapter.java
@@ -4,9 +4,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class PagerAdapter extends FragmentPagerAdapter {
+import com.astuetz.PagerSlidingTabStrip;
+
+public class PagerAdapter extends FragmentPagerAdapter implements PagerSlidingTabStrip.IconTabProvider {
 
     private static final String[] TABS_HEADER = {"Home", "YouTube", "Geolocation", "Shaking", "Settings"};
+    private static final int[] TABS_ICONS = {R.drawable.ic_tabs_home, R.drawable.ic_tabs_youtube, R.drawable.ic_tabs_geolocation, R.drawable.ic_tabs_accelerometer, R.drawable.ic_tabs_settings};
 
     public PagerAdapter(FragmentManager fm) {
         super(fm);
@@ -41,8 +44,14 @@ public class PagerAdapter extends FragmentPagerAdapter {
         return TABS_HEADER.length;
     }
 
+
     @Override
     public CharSequence getPageTitle(int position) {
         return TABS_HEADER[position];
+    }
+
+    @Override
+    public int getPageIconResId(int position) {
+        return TABS_ICONS[position];
     }
 }
