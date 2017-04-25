d63ade54e13f5f1be92cc495dc56d962654b2cfa
Mathieu Boily <mathiboily@hotmail.com>
2017-04-25 11:28:51 -0400

    Clean

--- AlarmeDeluxe/app/src/main/java/ca/ulaval/ima/mp/alarmedeluxe/youtube/YoutubeAlarmActivity.java
+++ AlarmeDeluxe/app/src/main/java/ca/ulaval/ima/mp/alarmedeluxe/youtube/YoutubeAlarmActivity.java
@@ -108,20 +108,6 @@ public class YoutubeAlarmActivity extends YouTubeBaseActivity implements GoogleA
         youTubePlayerView.initialize(getResources().getString(R.string.api_key),onInitializedListener);
     }
 
-    /** Authorizes the installed application to access user's protected data. */
-    /*private static Credential authorize() throws Exception {
-        // load client secrets
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-                new InputStreamReader(CalendarSample.class.getResourceAsStream("/client_secrets.json")));
-        // set up authorization code flow
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, JSON_FACTORY, clientSecrets,
-                Collections.singleton(CalendarScopes.CALENDAR)).setDataStoreFactory(dataStoreFactory)
-                .build();
-        // authorize
-        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-    }*/
-
     @Background
     public void getVideoRating(){
         new YoutubeVideoRatingFetch(this, getApplicationContext()).execute(url);
